6c06bbea05f05e6250dc9cfee019637722002277
Ankon Bhowmick <56348159+Ankon1708@users.noreply.github.com>
2025-07-01 01:30:59 +0530

    Update RAG_Mod.py

--- RAG_Mod.py
+++ RAG_Mod.py
@@ -60,13 +60,15 @@ The question is: {question}""",
 
 prompt_rag = ChatPromptTemplate.from_messages([system_prompt, prompt_rag_human])
 
-prompt_genq = ChatPromptTemplate.from_template(
+prompt_genq_human = HumanMessagePromptTemplate.from_template(
     """You are an AI language model assistant. Your task is to generate five 
 different versions of the given user question to retrieve relevant documents from a vector 
 database. By generating multiple perspectives on the user question, your goal is to help
 the user overcome some of the limitations of the distance-based similarity search. 
 Provide these alternative questions separated by newlines. Original question: {question}"""
 )
+
+prompt_genq = ChatPromptTemplate.from_messages([system_prompt, prompt_genq_human])
 
 prompt_final_human=HumanMessagePromptTemplate.from_template('''
 You are given information about the candidate that is relevant to the question. Here's the information : {context}
