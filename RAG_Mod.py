0d4f935df7498055853e01c60a3026ffc8434102
Ankon Bhowmick <56348159+Ankon1708@users.noreply.github.com>
2025-07-01 01:02:12 +0530

    V2
    PortfoliAI.v2: Multi-query parallel runnable architecture

---
+++ RAG_Mod.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 30 20:26:24 2025
+
+@author: ASUS
+"""
+
+#%% Import API Keys, libraries, set environment for LangSmith
+
+from Keys import my_lang_api_key, my_google_api_key
+import os
+os.environ["GOOGLE_API_KEY"] = my_google_api_key
+os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
+os.environ['LANGSMITH_PROJECT']="PortfoliAI_v1"
+os.environ['LANGCHAIN_API_KEY'] = my_lang_api_key
+
+import langchain
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.vectorstores import FAISS
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+from langchain_core.runnables.base import RunnableEach
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+from IPython.display import Markdown
+
+
+#%% Initialise LLM and embeddings, import VDB
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.5,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+
+vector_db=FAISS.load_local("faiss_vector_db",embeddings,allow_dangerous_deserialization=True)
+
+
+#%% Define Prompts
+
+system_prompt = SystemMessagePromptTemplate.from_template(
+    "You are an AI assistant that answers queries about a candidate."
+)
+
+prompt_rag_human = HumanMessagePromptTemplate.from_template(
+    """You are tasked with drafting an answer for a question asked regarding a candidate's skills and experience.
+
+The answer should only contain information on the candidate in the form of context that has been made available to you, do not falsify any skills or experience. 
+For behavourial questions, use slightly creative vocabulary to best answer them.
+The context is: {context}
+The question is: {question}""",
+    input_variables=["question"]
+)
+
+prompt_rag = ChatPromptTemplate.from_messages([system_prompt, prompt_rag_human])
+
+prompt_genq = ChatPromptTemplate.from_template(
+    """You are an AI language model assistant. Your task is to generate five 
+different versions of the given user question to retrieve relevant documents from a vector 
+database. By generating multiple perspectives on the user question, your goal is to help
+the user overcome some of the limitations of the distance-based similarity search. 
+Provide these alternative questions separated by newlines. Original question: {question}"""
+)
+
+prompt_final_human=HumanMessagePromptTemplate.from_template('''
+You are given information about the candidate that is relevant to the question. Here's the information : {context}
+Answer this question about the candidate by coherently restructuring the information while trying to reduce redundancy : {question}''')
+
+prompt_final=ChatPromptTemplate.from_messages([system_prompt, prompt_final_human])
+
+
+#%% Define Retriever and Chains
+
+retriever=vector_db.as_retriever(search_kwargs={"k": 3})
+
+# Basic RAG Chain using a query, and a retriever for context
+rag_chain = (
+    {"context": retriever, "question": RunnablePassthrough()}
+    | prompt_rag
+    | llm
+    | StrOutputParser()
+)
+
+# Generate multiple queries for wider semantic search
+generate_queries = (
+    prompt_genq
+    | llm 
+    | StrOutputParser() 
+    | (lambda x: x.split("\n"))
+)
+
+# Run the generated queries individually through rag chain and combine all answers
+parallel_rag=(
+    RunnableEach(bound=rag_chain)
+    | (lambda x: "".join(x))
+)
+
+# CC: Complete Chain
+# Use combined answer as context and generate a final answer
+CC_RAG=(                                
+    {'context':(generate_queries
+               | parallel_rag),
+     'question':RunnablePassthrough()
+    }
+    | prompt_final
+    | llm
+    | StrOutputParser()
+)
+
+
+#%% Define function for modular use
+
+def My_Chatbot(query):
+    return CC_RAG.invoke(query)
--- main.py
+++ main.py
@@ -1,53 +1,14 @@
-from Keys import my_google_api_key
-import os
-os.environ["GOOGLE_API_KEY"] = my_google_api_key
-
-from langchain_google_genai import ChatGoogleGenerativeAI
-import langchain
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.0-flash",
-    temperature=0.5,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2,
-    # other params...
-)
-
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
-from langchain.vectorstores import FAISS
-from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
-
-embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-
-vector_db=FAISS.load_local("faiss_vector_db",embeddings,allow_dangerous_deserialization=True)
+import RAG_Mod
 
 import streamlit as st
-import time
 
 st.title("PortfoliAI")
-github_url = "https://github.com/Ankon1708"
-linkedin_url = "https://www.linkedin.com/in/ankon-bhowmick-2a8805193/"
-
 st.sidebar.title("I am Ankon Bhowmick, an aspiring data scientist.\n This is an RAG-based chat bot that demonstrates my new-found skills in Generative AI.\n This bot is capable of answering questions about my academic journey, internship experience, and my entire portfolio of projects and skills.\n Feel free to ask any question pertaining to them.")
-st.sidebar.markdown("### 🔗 Connect")
-st.sidebar.markdown(f"[![GitHub](https://img.shields.io/badge/GitHub-Profile-blue?logo=github)]({github_url})")
-st.sidebar.markdown(f"[![LinkedIn](https://img.shields.io/badge/LinkedIn-Profile-blue?logo=linkedin)]({linkedin_url})")
-with open("Ankon Bhowmick Resume.pdf", "rb") as file:
-    st.sidebar.download_button("Download CV", file, file_name="CV.pdf")
 
 main_placeholder = st.empty()
 query = main_placeholder.text_input("Question: ")
-sys_prompt='''You are an AI assistant that answers queries about Ankon Bhowmick's academic and internship experience. 
-            Answer the questions while framing the candidate's experience and skills in the most positive way 
-            but do not make up any information beyond what is provided. The question is: '''
-if query:
-    query=sys_prompt+query
-    chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vector_db.as_retriever())
-        
-    result=chain({"question": query}, return_only_outputs=True)
-        
-    st.header("Answer")
-    st.write(result["answer"])
-    
 
+if query:
+    result=RAG_Mod.My_Chatbot(query)
+    st.header("Answer")
+    st.write(result)
